60341445e3db91e4afd321be5566ffc3eca34eb0
bekoszn <berkeozguder@gmail.com>
2024-10-06 14:16:36 +0300

    Initial Commit

---
+++ BitirmeProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitirmeProjesi
+//
+//  Created by Berke Özgüder on 6.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BitirmeProjesiTests/BitirmeProjesiTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitirmeProjesiTests.swift
+//  BitirmeProjesiTests
+//
+//  Created by Berke Özgüder on 6.10.2024.
+//
+
+import Testing
+@testable import BitirmeProjesi
+
+struct BitirmeProjesiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
